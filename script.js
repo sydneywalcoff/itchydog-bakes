643c7fbc881d66c57af72e69ac548ff06c7ff8da
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-03-19 12:45:27 -0700

    add script.js and outline plan

---
+++ script.js
@@ -0,0 +1,12 @@
+// validate email
+
+
+// dropdown functionality
+    // arrow flips down
+
+
+// radio btn functionality
+
+
+// submit btn functionality
+    // error messaging if missing required fields
