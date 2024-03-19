69907f493f6c67aa746175bf23ae0de1ff0373b8
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-03-19 15:21:06 -0700

    add basic input validation

--- script.js
+++ script.js
@@ -25,18 +25,38 @@ function handleRadioBtnChange() {
 
 $('.radio-btn').on('click', handleRadioBtnChange);
 
+// generate Error Messaging
+function generateErrorMessaging(vals) {
+    let errorText;
+    if(vals.length == 1) {
+        errorText = `${vals[0]} is a required field`;
+    } else {
+        errorText = `${vals[0]} and ${vals[1]} are required fields`;
+    }
+
+    $('form .error').text(errorText)
+}
+
 // gather formData 
 function gatherFormData() {
+    const emptyVals = [];
     const name = $('#name-input').val();
     const email = $('#email-input').val();
+    if (!name) emptyVals.push('name');
+    if (!email) emptyVals.push('email');
     const other = $('#other-textbox').val();
-    const shippingOrPickup = $('#shipping-btn').attr('aria-checked') == 'true' ? 'shipping' : 'pickup' ;
+    const shippingOrPickup = $('#shipping-btn').attr('aria-checked') == 'true' ? 'shipping' : 'pickup';
     const selectedQty = $('#dropdown .selected .option').text();
+
+    if (emptyVals) {
+        generateErrorMessaging(emptyVals);
+        return {};
+    }
 
     return {
         name,
         email,
-        other, 
+        other,
         shippingOrPickup,
         selectedQty
     }
@@ -44,10 +64,9 @@ function gatherFormData() {
 
 
 // submit btn functionality
-$('#submit-btn').on('click', function(e){
+$('#submit-btn').on('click', function (e) {
     e.preventDefault();
     const formData = gatherFormData();
 
-    console.log(formData)
     // error messaging if missing required fields
 })
